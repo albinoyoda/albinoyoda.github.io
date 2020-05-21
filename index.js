46145cc9c3c47d6f8c9079b5ce3b301351fbae13
Oliver Sundell <oliver.sundell@hotmail.com>
2020-05-21 11:14:53 +0200

    small changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-// const PORT = process.env.PORT || 5000
 //
 
 
@@ -7,6 +6,7 @@ var static = require('node-static');
 var file = new static.Server();
 const express = require('express')
 const path = require('path')
+const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'interface')))
