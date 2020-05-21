2841e15bea87fb974d21b81cd1c4da93b84f4d88
Oliver Sundell <oliver.sundell@hotmail.com>
2020-05-21 11:12:31 +0200

    small changes

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-// const express = require('express')
-// const path = require('path')
+
 // const PORT = process.env.PORT || 5000
 //
-// express()
-//   .use(express.static(path.join(__dirname, 'interface')))
-//   .set('view engine', 'html')
-//   .get('/', (req, res) => res.render('index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 var static = require('node-static');
 var file = new static.Server();
-require('http').createServer(function(request, response) {
-    request.addListener('end', function() {
-        file.serve(request, response);
-    }).resume();
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const path = require('path')
+
+express()
+  .use(express.static(path.join(__dirname, 'interface')))
+  .set('view engine', 'html')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// require('http').createServer(function(request, response) {
+//     request.addListener('end', function() {
+//         file.serve(request, response);
+//     }).resume();
+// }).listen(process.env.PORT || 3000);
