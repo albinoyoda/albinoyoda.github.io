afa7bb8c5c754dface35b5d4b2f7f4c5d2b970ae
Oliver Sundell <oliver.sundell@hotmail.com>
2020-05-29 14:08:22 +0200

    Fixed item names and off/main hand items to be selected properly

--- interface/parse_items.py
+++ interface/parse_items.py
@@ -9,6 +9,14 @@ def remove_blank_spaces(string_list):
     return washed_list
 
 
+def remove_underscores_and_capitalize(string):
+    split_string = string.split("_")
+    output = split_string[0].capitalize()
+    for i in range(1, len(split_string)):
+        output = output + " " + split_string[i]
+    return output
+
+
 def is_string_left_curly_bracket(string):
     split_string = string.split(' ')
     washed_string = remove_blank_spaces(split_string)
@@ -22,6 +30,15 @@ class Armor_category:
 
     def add_item(self, item):
         self.items.append(item)
+
+
+class Weapon_category:
+    def __init__(self, category):
+        self.category = category
+        self.items = []
+
+    def add_item(self, wep_name, wep_hand):
+        self.items.append(wep_name + "*" + wep_hand)
 
 
 file = "/../wow_library/include/Armory.hpp"
@@ -52,30 +69,41 @@ for line in lines:
 
 weapons = []
 found_item_vector = False
+found_item_socket = False
+found_item_name = False
 for line in lines:
     line1 = line.split(' ')
     line1 = remove_blank_spaces(line1)
     if line1[0] == 'std::vector<Weapon>':
         if line1[1][-3:-1] == '_t':
-            armor = Armor_category(line1[1][0:-3])
+            weapon = Weapon_category(line1[1][0:-3])
             found_item_vector = True
 
     if found_item_vector:
         line2 = line.split('"')
         if len(line2) == 3:
             if is_string_left_curly_bracket(line2[0]):
-                armor.add_item(line2[1])
+                weapon_name = line2[1]
+                found_item_name = True
+
+        line3 = line.split('Weapon_socket')
+        if len(line3) == 2:
+            weapon_hand = line3[1][2:5]
+            found_item_socket = True
+
+    if found_item_name and found_item_socket:
+        weapon.add_item(weapon_name, weapon_hand)
+        found_item_socket = False
+        found_item_name = False
 
     if line1[0] == '};\n' and found_item_vector:
         found_item_vector = False
-        weapons.append(armor)
+        weapons.append(weapon)
 
 # all_weapons = []
 for wep_type in weapons:
     wep_type.items = list(set(wep_type.items))
     wep_type.items.sort()
-    # for item in wep_type.items:
-    #     all_weapons.append(item)
 
 with open("index.html", "r") as f:
     lines = f.readlines()
@@ -117,7 +145,8 @@ if found_start and found_stop:
                 generated_1.append(
                     '    <option value="none" selected disabled>' + armor_type.category + str(i + 1) + '</option>\n')
                 for item in armor_type.items:
-                    generated_1.append('    <option value="' + item + '">' + item + '</option>\n')
+                    item_name = remove_underscores_and_capitalize(item)
+                    generated_1.append('    <option value="' + item + '">' + item_name + '</option>\n')
                 generated_1.append('</select>\n')
                 generated_1.append('\n')
                 dropdowns_in_a_row = dropdowns_in_a_row + 1
@@ -125,7 +154,8 @@ if found_start and found_stop:
             generated_1.append('<select id="' + armor_type.category + '_dd" >\n')
             generated_1.append('    <option value="none" selected disabled>' + armor_type.category + '</option>\n')
             for item in armor_type.items:
-                generated_1.append('    <option value="' + item + '">' + item + '</option>\n')
+                item_name = remove_underscores_and_capitalize(item)
+                generated_1.append('    <option value="' + item + '">' + item_name + '</option>\n')
             generated_1.append('</select>\n')
             generated_1.append('\n')
             dropdowns_in_a_row = dropdowns_in_a_row + 1
@@ -145,7 +175,15 @@ if found_start and found_stop:
             generated_2.append(
                 '    <option value="none" disabled> --- ' + wep_type.category.upper() + ' --- </option>\n')
             for item in wep_type.items:
-                generated_2.append('    <option value="' + item + '">' + item + '</option>\n')
+                split_item = item.split("*")
+                if i == 0:
+                    if split_item[1] == "one" or split_item[1] == "mai":
+                        item_name = remove_underscores_and_capitalize(split_item[0])
+                        generated_2.append('    <option value="' + split_item[0] + '">' + item_name + '</option>\n')
+                if i == 1:
+                    if split_item[1] == "one" or split_item[1] == "off":
+                        item_name = remove_underscores_and_capitalize(split_item[0])
+                        generated_2.append('    <option value="' + split_item[0] + '">' + item_name + '</option>\n')
         generated_2.append('</select>\n')
         generated_2.append('\n')
 
