8e58ef57178988516b8335df1497e31fdd4f73e7
Oliver Sundell <oliver.sundell@hotmail.com>
2020-09-25 23:10:35 +0200

    update npm

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-
-//
-
-var static = require('node-static');
-var file = new static.Server();
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-var tools = require('./interface/emscripten/wow_interface');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'interface')))
-  .set('view engine', 'html')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'interface')))
+    .set('view engine', 'html')
+    .get('/', (req, res) => res.render('index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// require('http').createServer(function(request, response) {
-//     request.addListener('end', function() {
-//         file.serve(request, response);
-//     }).resume();
-// }).listen(process.env.PORT || 3000);
