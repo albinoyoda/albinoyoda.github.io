77cc92776a570f6e0bf0fe7fb63b3c2ca43926b4
Denis <shepetov.denis@gmail.com>
2020-10-12 21:20:09 -0700

    Create docker container

--- docker_cmake.ps1
+++ docker_cmake.ps1
@@ -1,3 +1,6 @@
 Remove-Item ./interface/emscripten -Recurse -ErrorAction Ignore
 mkdir -p interface/emscripten
-docker run --rm -v ${PWD}:/src trzeci/emscripten /bin/bash -c "cd interface/emscripten &&  emconfigure cmake ../.. -DEMSCRIPTEN=True && emmake make"
+docker run --rm -v ${PWD}:/src trzeci/emscripten /bin/bash -c "cd interface/emscripten &&  emcmake cmake ../.. -DEMSCRIPTEN=True && emmake make"
+
+# Complie docker container
+docker build -t rzeci/emscripten .
