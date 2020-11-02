a94a1ccccd74ac6b5f741c97f5a19ae9c10dbd70
Denis <shepetov.denis@gmail.com>
2020-11-02 09:34:55 -0800

    Enable iterative build

--- docker_cmake.ps1
+++ docker_cmake.ps1
@@ -1,4 +1,3 @@
-Remove-Item ./interface/emscripten -Recurse -ErrorAction Ignore
 mkdir -p interface/emscripten
 docker run --rm -v ${PWD}:/src trzeci/emscripten /bin/bash -c "cd interface/emscripten &&  emcmake cmake ../.. -DEMSCRIPTEN=True && emmake make"
 
