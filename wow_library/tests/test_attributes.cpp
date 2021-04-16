f38838cec2cddfdbf372b87b2197a6d5e92cd61b
The Grox Empire <47816228+TheGroxEmpire@users.noreply.github.com>
2021-04-16 16:37:24 +0700

    Update test_attributes.cpp

--- wow_library/tests/test_attributes.cpp
+++ wow_library/tests/test_attributes.cpp
@@ -4,12 +4,12 @@
 
 TEST(TestSuite, test_attributes)
 {
-    const auto a = Attributes{5, 10};
-    const auto b = Attributes{15, 20};
+    const auto a = Attributes{5, 16.5};
+    const auto b = Attributes{15, 33};
     const auto c = a + b;
 
     ASSERT_FLOAT_EQ(c.strength, 20);
-    ASSERT_FLOAT_EQ(c.agility, 30);
+    ASSERT_FLOAT_EQ(c.agility, 49.5);
 
     auto st = Special_stats{};
     st.stat_multiplier = 0.5;
@@ -17,7 +17,7 @@ TEST(TestSuite, test_attributes)
     auto multiplied = a.multiply(st);
 
     ASSERT_FLOAT_EQ(multiplied.strength, 7.5);
-    ASSERT_FLOAT_EQ(multiplied.agility, 15);
+    ASSERT_FLOAT_EQ(multiplied.agility, 24.75);
 
     st = b.convert_to_special_stats(st);
 
